c1f8e2f6816cd4f936833964e2d51272c41fc6f8
Alisa Ahokas <alisa.vik@hotmail.com>
2023-03-06 18:59:00 +0200

    tester.py send resvdata and persondata to api, not finished

--- tester.py
+++ tester.py
@@ -27,6 +27,11 @@ def detection():
     c.execute("SELECT * FROM game WHERE name=?", (names[1],))
     game_data = c.fetchone()
 
+    #to empty reaervation table during testing
+    c.execute("DELETE FROM reservation;")
+    # Commit the changes
+    conn.commit()
+
     # Insert reservation data
     c.execute(
         "INSERT INTO reservation (id, person_id, game_id, start_date, end_date) VALUES (?, ?, ?, '06032023', '07032023')",
@@ -39,12 +44,24 @@ def detection():
 
     print(reservation_data)
 
+
+    # find data with queries
+    # use person_id and game_id to create a new reservation query
+
+    # query for person, save data to variable
+    # query for reservations with person id, save data to variable
+
+    # add person data and reservation data to api/detections
+    # data = {'person': person_data, 'reservations': reservation_data}
     data = []
-    for name in names:
-        c.execute("SELECT * FROM person WHERE name=?", (name,))
-        results = c.fetchall()
-        for result in results:
-            data.append(result)
+    if len(reservation_data) > 1:
+        for unique_reservation_data in reservation_data:
+            c.execute("SELECT * FROM reservation WHERE id=?", (1,))
+            results = c.fetchall()
+            for result in results:
+                data.append(result)
+    else:
+        data.append(reservation_data)
 
 
     # Return the recognition results as a JSON response
