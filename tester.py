cae9a4cbd755378d3793af9a69cd6eacf9884e1f
Pauli Hemmi <lolled.mr@gmail.com>
2023-03-09 12:19:31 +0200

    continued previous python code

--- tester.py
+++ tester.py
@@ -5,20 +5,14 @@ import sqlite3
 app = Flask(__name__)
 CORS(app)
 
-print("test first")
 conn = sqlite3.connect('mydatabase.db', check_same_thread=False)
-
 c = conn.cursor()
-
-
-
 
 @app.route('/api/detection', methods=['GET'])
 def detection():
     # Call the main.py script
 
-    names = ['Pauli', 'Monopoly']
-
+    names = ['Pauli', 'Monopoly', 'Alias']
     # Retrieve person data
     c.execute("SELECT * FROM person WHERE name=?", (names[0],))
     person_data = c.fetchone()
@@ -27,47 +21,50 @@ def detection():
     c.execute("SELECT * FROM game WHERE name=?", (names[1],))
     game_data = c.fetchone()
 
-    #to empty reaervation table during testing
+
+    # Retrieve game data
+    c.execute("SELECT * FROM game WHERE name=?", (names[2],))
+    game_data2 = c.fetchone()
+
+    # Delete all rows in the reservation table
     c.execute("DELETE FROM reservation;")
-    # Commit the changes
     conn.commit()
 
     # Insert reservation data
-    c.execute(
-        "INSERT INTO reservation (id, person_id, game_id, start_date, end_date) VALUES (?, ?, ?, '06032023', '07032023')",
-        (1, person_data[0], game_data[0]))
+    c.execute("INSERT INTO reservation (id, person_id, game_id, start_date, end_date) VALUES (?, ?, ?, '06032023', '07032023')",
+              (1, person_data[0], game_data[0]))
+    c.execute("INSERT INTO reservation (id, person_id, game_id, start_date, end_date) VALUES (?, ?, ?, '06032023', '07032023')",
+              (2, person_data[0], game_data2[0]))
     conn.commit()
 
-    # Retrieve reservation data
-    c.execute("SELECT * FROM reservation WHERE id=?", (1,))
-    reservation_data = c.fetchone()
+    c.execute("SELECT * FROM reservation")
+    reservation_data = c.fetchall()
 
     print(reservation_data)
 
 
-    # find data with queries
-    # use person_id and game_id to create a new reservation query
-
-    # query for person, save data to variable
-    # query for reservations with person id, save data to variable
-
-    # add person data and reservation data to api/detections
-    # data = {'person': person_data, 'reservations': reservation_data}
+    # Create a list to store the reservation data
     data = []
-    if len(reservation_data) > 1:
-        for unique_reservation_data in reservation_data:
-            c.execute("SELECT * FROM reservation WHERE id=?", (1,))
-            results = c.fetchall()
-            for result in results:
-                data.append(result)
-    else:
-        data.append(reservation_data)
+    for row in reservation_data:
+        # Create a dictionary to store the reservation data labeled by their names
+        reservation = {
+            'id': row[0],
+            'person_id': row[1],
+            'game_id': row[2],
+            'start_date': row[3],
+            'end_date': row[4]
+        }
+
+        # 
 
 
-    # Return the recognition results as a JSON response
-    return jsonify({'detection': data})
 
+        # Append the reservation data to the list
+        data.append(reservation)
+        data.append(person)
+
+    # Return the reservation data as a JSON response
+    return jsonify({'reservations': data})
 
 if __name__ == '__main__':
-    app.debug = True
-    app.run(port=5001)
+    app.run(debug=True, port=5001)
