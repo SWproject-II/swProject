81025f1997abeb7e0bf146364e897ebd3954a15e
Alisa Ahokas <alisa.vik@hotmail.com>
2023-03-13 15:16:46 +0200

    fronted getting data from api and correcting port to 5001

--- frontend/src/Constants.js
+++ frontend/src/Constants.js
@@ -1,3 +1,3 @@
-export const API_getDetection = "http://localhost:5000/api/detection";
+export const API_getDetection = "http://localhost:5001/api/detection";
 export const MOCK_API =
   "https://run.mocky.io/v3/9b816442-c0cc-4043-b0ca-39c34fb31f4e"; // mocky api for testing
--- frontend/src/components/Face.js
+++ frontend/src/components/Face.js
@@ -2,23 +2,31 @@ import React, { useState, useEffect } from "react";
 import { API_getDetection } from "../Constants";
 
 export default function Face() {
-  const [detection, setDetection] = useState("");
+  //const [detection, setDetection] = useState("");
+  const [name, setName] = useState("");
+  const [reservations, setReservations] = useState([]);
 
   useEffect(() => {
     fetch(API_getDetection)
       .then((response) => response.json())
-      .then((data) => setDetection(data.detection))
-      .catch((error) => console.error(error));
+      .then(data => {
+        setName(data.data.person.name);
+        setReservations(data.data.reservations);
+      })
+      .catch(error => console.error(error));
   }, []);
 
   return (
-    <>
-      <div>
-        {" "}
-        <p>
-          {detection[0]}detection 1 ja detection 2 {detection[1]}
-        </p>
-      </div>
-    </>
+    <div>
+      {" "}
+      <h3>{name}'s Reservations:</h3>
+      <ul>
+        {reservations.map(reservation => (
+          <li key={reservation.id}>
+            Game id: {reservation.game_id}, Start date: {reservation.start_date}, End date: {reservation.end_date}
+          </li>
+        ))}
+      </ul>
+    </div>
   );
 }
